17aa2306ea6cb3738d5a2454661308ee01a5c1a9
Julien <julien.proudy@gmail.com>
2021-04-03 18:18:30 +0200

    Ajout de commentaire et changement de nom de fichier

--- dashproject/dashfront/src/app/components/events/events.component.ts
+++ dashproject/dashfront/src/app/components/events/events.component.ts
@@ -1,11 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { ITS_JUST_ANGULAR } from '@angular/core/src/r3_symbols';
-import { AppServiceService } from 'src/app/app-service.service';
 
-var lieu :String;
+/* Créaton de la fonction changement d'événement */
 declare const changerEvent: any;
-
 
 @Component({
   selector: 'app-events',
@@ -14,6 +11,7 @@ declare const changerEvent: any;
 })
 
 export class EventsComponent implements OnInit {
+  /* Variable de stockage sous forme de tableau des événements */
   events = {
     evenement1: [" ", " "," ", " ", " "],
     evenement2: [" ", " "," ", " ", " "],
@@ -22,37 +20,36 @@ export class EventsComponent implements OnInit {
   totalAngularPackages: any;
   constructor(private http: HttpClient) {}
 
+  /* Fonction qui s'active lors d'un click sur le bouton souhaité, ici le bouton "suivant" et "précédent" */
   onClick(sens: any){
     changerEvent(sens,this.events.evenement1,this.events.evenement2,this.events.evenement3);
   }
 
   ngOnInit() {
+    /* Lien vers l'API */
     this.http.get<any>('https://public.opendatasoft.com/api/records/1.0/search/?dataset=evenements-publics-cibul&q=&facet=tags&facet=placename&facet=date_start&facet=date_end&facet=pricing_info&facet=updated_at&facet=city_district&refine.placename=Le+Mans&refine.date_start=2021').subscribe(data => {
       this.totalAngularPackages = data.total;
 
-      var tab=new Array();
-
+      /* Affichage des données dans la console du site (inspecter l'élement) */
       console.log(data);
     
-      this.events.evenement1[0] = data.records[0].fields.title;
-      this.events.evenement1[1] = data.records[0].fields.date_start;
-      this.events.evenement1[2] = data.records[0].fields.address;
-      this.events.evenement1[3] = data.records[0].fields.image;
-      this.events.evenement1[4] = data.records[0].fields.description;
+      this.events.evenement1[0] = data.records[0].fields.title;       /* Récupération du titre de l'événement */
+      this.events.evenement1[1] = data.records[0].fields.date_start;  /* Récupération de la date de début de l'événement */
+      this.events.evenement1[2] = data.records[0].fields.address;     /* Récupération de l'adresse de l'événement */
+      this.events.evenement1[3] = data.records[0].fields.image;       /* Récupération de l'image de l'événement */
+      this.events.evenement1[4] = data.records[0].fields.description; /* Récupération de la description de l'événement */
 
-      this.events.evenement2[0] = data.records[1].fields.title;
-      this.events.evenement2[1] = data.records[1].fields.date_start;
-      this.events.evenement2[2] = data.records[1].fields.address;
-      this.events.evenement2[3] = data.records[1].fields.image;
-      this.events.evenement2[4] = data.records[1].fields.description;
+      this.events.evenement2[0] = data.records[1].fields.title;       /* Récupération du titre de l'événement */
+      this.events.evenement2[1] = data.records[1].fields.date_start;  /* Récupération de la date de début de l'événement */
+      this.events.evenement2[2] = data.records[1].fields.address;     /* Récupération de l'adresse de l'événement */
+      this.events.evenement2[3] = data.records[1].fields.image;       /* Récupération de l'image de l'événement */
+      this.events.evenement2[4] = data.records[1].fields.description; /* Récupération de la description de l'événement */
 
-      this.events.evenement3[0] = data.records[2].fields.title;
-      this.events.evenement3[1] = data.records[2].fields .date_start;
-      this.events.evenement3[2] = data.records[2].fields.address;
-      this.events.evenement3[3] = data.records[2].fields.image;
-      this.events.evenement3[4] = data.records[2].fields.description;
-
-
+      this.events.evenement3[0] = data.records[2].fields.title;       /* Récupération du titre de l'événement */
+      this.events.evenement3[1] = data.records[2].fields .date_start; /* Récupération de la date de début de l'événement */
+      this.events.evenement3[2] = data.records[2].fields.address;     /* Récupération de l'adresse de l'événement */
+      this.events.evenement3[3] = data.records[2].fields.image;       /* Récupération de l'image de l'événement */
+      this.events.evenement3[4] = data.records[2].fields.description; /* Récupération de la description de l'événement */
 
     })
   }
--- dashproject/dashfront/src/assets/js/evenement.js
+++ dashproject/dashfront/src/assets/js/evenement.js
@@ -1,8 +1,10 @@
-num_event= 1;
+num_event = 1;
 
+/* Systeme de changement d'évenement en fonction d'un sens qui est défini par le bouton "suivant" ou "précédent" */
 function changerEvent(sens, evenement1, evenement2, evenement3){
-    //Si "etat" est a true alors le sens est vers l'avant sinon il est vers l'arrière
-    if(sens == 1){ //Si le sens est égale à 1 (vers l'avant), alors on incrémente le numéro d'evenement
+
+    /* Si le sens est égale à 1 (vers l'avant), alors on incrémente le numéro d'evenement */
+    if(sens == 1){ 
         if(num_event == 1){ 
             num_event = 2;
         }else if(num_event == 2){
@@ -10,7 +12,8 @@ function changerEvent(sens, evenement1, evenement2, evenement3){
         }else if(num_event == 3){
             num_event = 1;
         }
-    }else if(sens == -1){    //Si le sens est égal à -1 (vers l'arrières), alors on décrémente le numéro d'evenement
+    /* Si le sens est égal à -1 (vers l'arrières), alors on décrémente le numéro d'evenement */
+    }else if(sens == -1){ //
         if(num_event == 1){
            num_event = 3;
         }else if(num_event == 2){
@@ -20,6 +23,7 @@ function changerEvent(sens, evenement1, evenement2, evenement3){
         }
     }
     
+    /* Affichage de l'évènement 1 */
     if(num_event == 1){
         document.getElementById('titre').innerHTML = evenement1[0];
         document.getElementById('date').innerHTML = evenement1[1];
@@ -29,6 +33,7 @@ function changerEvent(sens, evenement1, evenement2, evenement3){
 
         document.getElementById('cmp').innerHTML = "1/3";
     
+    /* Affichage de l'évènement 2 */
     }else if(num_event == 2){
         document.getElementById('titre').innerHTML = evenement2[0];
         document.getElementById('date').innerHTML = evenement2[1];
@@ -37,7 +42,8 @@ function changerEvent(sens, evenement1, evenement2, evenement3){
         document.getElementById('description').innerHTML = evenement2[4];
             
         document.getElementById('cmp').innerHTML = "2/3";
-
+    
+    /* Affichage de l'évènement 3 */
     }else if(num_event == 3){
         document.getElementById('titre').innerHTML = evenement3[0];
         document.getElementById('date').innerHTML = evenement3[1];
